3be344759b137c67c1fda204a43df334325e3ba0
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-30 06:56:11 +0000

    Format C/C++ code with clang-format

--- src/debug.h
+++ src/debug.h
@@ -3,29 +3,27 @@
  * Github: https://www.github.com/awesomelewis2007/emulator
  * Licence: GPL-3.0
  * By: Lewis Evans
-*/
+ */
 
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <iomanip>
 
-#include "lib/nice_verbose/nice_verbose.h"
 #include "lib/console_colour/colour.h"
+#include "lib/nice_verbose/nice_verbose.h"
 
 #include "main.h"
 
 using namespace std;
 
-
 /**
  * call_debugger() - Calls the debugger if DEBUG is true
  * @message: The message to display
  * @returns: void
-*/
-void call_debugger(string message)
-{
-    if(DEBUG){
+ */
+void call_debugger(string message) {
+    if (DEBUG) {
         nice_verbose_text::info(message);
     }
 }
@@ -35,46 +33,39 @@ void call_debugger(string message)
  * @memory: The memory to dump
  * @end: The end of the memory to dump, defaults to 256
  * @returns: void
- * 
- * Dumps the memory in a nice format for debugging. The memory 
+ *
+ * Dumps the memory in a nice format for debugging. The memory
  * is dumped in 8 byte chunks.
- * 
+ *
  * Example:
  * 00 00 00 00 00 00 00 00
  * 00 00 00 00 00 00 00 00
  * 00 00 00 00 00 00 00 00
  * 00 00 00 00 00 00 00 00
  * 00 00 00 00 00 00 00 00
- * 
- * 
-*/
-void memory_dump(vector<int> memory, int end = 256)
-{
+ *
+ *
+ */
+void memory_dump(vector<int> memory, int end = 256) {
     /* Dumps the memory in a nice format for debugging */
     bool first = true;
     int ln = 0;
 
-    for (int i = 0; i < end; i++)
-    {
-        if (i % 8 == 0)
-        {
-            
+    for (int i = 0; i < end; i++) {
+        if (i % 8 == 0) {
+
             /* prevents a new line at the start of the dump */
-            if (first)
-            {
+            if (first) {
                 first = false;
-            }
-            else
-            {
+            } else {
                 cout << endl;
             }
         }
-        
+
         cout << setfill('0') << setw(2) << hex << memory[i] << " ";
     }
     cout << endl;
 }
-
 
 /**
  * reg_dump() - Dumps the registers in a nice format for debugging
@@ -85,9 +76,8 @@ void memory_dump(vector<int> memory, int end = 256)
  * @returns: void
  *
  * Dumps the registers in a nice format for debugging.
-*/
-void reg_dump(int a,int b,int c, int d)
-{
+ */
+void reg_dump(int a, int b, int c, int d) {
     cout << "A: " << setfill('0') << setw(2) << hex << a << " ";
     cout << "B: " << setfill('0') << setw(2) << hex << b << " ";
     cout << "C: " << setfill('0') << setw(2) << hex << c << " ";
--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,50 @@
  * Github: https://www.github.com/awesomelewis2007/emulator
  * Licence: GPL-3.0
  * By: Lewis Evans
-*/
+ */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <iomanip>
-#include <vector>
 #include <algorithm>
-#include <iterator>
+#include <bitset>
 #include <cctype>
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include <cmath>
-#include <map>
-#include <set>
-#include <list>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <numeric>
-#include <utility>
+#include <fstream>
 #include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
 
-#include "lib/nice_verbose/nice_verbose.h"
 #include "lib/console_colour/colour.h"
+#include "lib/nice_verbose/nice_verbose.h"
 
 #include "debug.h"
 #include "main.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    if(argv[1] == string("--version") || argv[1] == string("-v"))
-    {
+int main(int argc, char *argv[]) {
+    if (argv[1] == string("--version") || argv[1] == string("-v")) {
         cout << "Emulator version " << VERSION << endl;
         return 0;
     }
-    if (argv[1] == string("--help") || argv[1] == string("-h"))
-    {
+    if (argv[1] == string("--help") || argv[1] == string("-h")) {
         cout << "Usage: emulator [options] [program file]" << endl;
         cout << "Options:" << endl;
         cout << "  -h, --help     Show this help message" << endl;
         cout << "  -v, --version  Show the version of the emulator" << endl;
     }
-    if (argc < 2)
-    {
+    if (argc < 2) {
         cout << "Please provide a program file" << endl;
         return 1;
     }
@@ -68,243 +64,194 @@ int main(int argc, char *argv[])
     int reg_d = 0;
 
     int location = 0;
-    while(getline(file, line))
-    {
-        for(int i = 0; i < line.length(); i += 2)
-        {
+    while (getline(file, line)) {
+        for (int i = 0; i < line.length(); i += 2) {
             instructions.push_back(line.substr(i, 2));
         }
     }
-    for (int i = 0; i < instructions.size(); i++)
-    {
-        call_debugger(to_string(i)   + " -> " + instructions[i]);
-        if (instructions[i] == "00")
-        {
-        }
-        else if (instructions[i] == "01")
-        {
+    for (int i = 0; i < instructions.size(); i++) {
+        call_debugger(to_string(i) + " -> " + instructions[i]);
+        if (instructions[i] == "00") {
+        } else if (instructions[i] == "01") {
             int location = stoi(instructions[i + 1], nullptr, 16);
             call_debugger("SELECT " + to_string(location));
             i++;
-        }
-        else if (instructions[i] == "02")
-        {
+        } else if (instructions[i] == "02") {
             int number = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("PUT " + to_string(number) + " -> " + to_string(location));
+            call_debugger("PUT " + to_string(number) + " -> " +
+                          to_string(location));
             memory[location] = number;
             i++;
-        }
-        else if (instructions[i] == "03")
-        {
+        } else if (instructions[i] == "03") {
             int number = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("ADD " + to_string(number) + " -> " + to_string(location));
+            call_debugger("ADD " + to_string(number) + " -> " +
+                          to_string(location));
             memory[location] = memory[location] + number;
             i++;
-        }
-        else if (instructions[i] == "04")
-        {
+        } else if (instructions[i] == "04") {
             int number = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("MIN " + to_string(number) + " -> " + to_string(location));
+            call_debugger("MIN " + to_string(number) + " -> " +
+                          to_string(location));
             memory[location] = memory[location] - number;
             i++;
-        }
-        else if (instructions[i] == "05")
-        {
+        } else if (instructions[i] == "05") {
             int number = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("MUL " + to_string(number) + " -> " + to_string(location));
+            call_debugger("MUL " + to_string(number) + " -> " +
+                          to_string(location));
             memory[location] = memory[location] * number;
             i++;
-        }
-        else if (instructions[i] == "06")
-        {
+        } else if (instructions[i] == "06") {
             int number = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("DIV " + to_string(number) + " -> " + to_string(location));
+            call_debugger("DIV " + to_string(number) + " -> " +
+                          to_string(location));
             memory[location] = memory[location] / number;
             i++;
-        }
-        else if (instructions[i] == "1A")
-        {
+        } else if (instructions[i] == "1A") {
             reg_a = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("SET A " + to_string(reg_a) + " -> " + to_string(location));
+            call_debugger("SET A " + to_string(reg_a) + " -> " +
+                          to_string(location));
             i++;
-        }
-        else if (instructions[i] == "1B")
-        {
+        } else if (instructions[i] == "1B") {
             reg_b = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("SET B " + to_string(reg_b) + " -> " + to_string(location));
+            call_debugger("SET B " + to_string(reg_b) + " -> " +
+                          to_string(location));
             i++;
-        }
-        else if (instructions[i] == "1C")
-        {
+        } else if (instructions[i] == "1C") {
             reg_c = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("SET C " + to_string(reg_c) + " -> " + to_string(location));
+            call_debugger("SET C " + to_string(reg_c) + " -> " +
+                          to_string(location));
             i++;
-        }
-        else if (instructions[i] == "1D")
-        {
+        } else if (instructions[i] == "1D") {
             reg_d = stoi(instructions[i + 1], nullptr, 16);
-            call_debugger("SET D " + to_string(reg_d) + " -> " + to_string(location));
+            call_debugger("SET D " + to_string(reg_d) + " -> " +
+                          to_string(location));
             i++;
-        }
-        else if (instructions[i] == "2A")
-        {
+        } else if (instructions[i] == "2A") {
             reg_a = memory[location];
-            call_debugger("GET A " + to_string(reg_a) + " <- " + to_string(location));
-        }
-        else if (instructions[i] == "2B")
-        {
+            call_debugger("GET A " + to_string(reg_a) + " <- " +
+                          to_string(location));
+        } else if (instructions[i] == "2B") {
             reg_b = memory[location];
-            call_debugger("GET B " + to_string(reg_b) + " <- " + to_string(location));
-        }
-        else if (instructions[i] == "2C")
-        {
+            call_debugger("GET B " + to_string(reg_b) + " <- " +
+                          to_string(location));
+        } else if (instructions[i] == "2C") {
             reg_c = memory[location];
-            call_debugger("GET C " + to_string(reg_c) + " <- " + to_string(location));
-        }
-        else if (instructions[i] == "2D")
-        {
+            call_debugger("GET C " + to_string(reg_c) + " <- " +
+                          to_string(location));
+        } else if (instructions[i] == "2D") {
             reg_d = memory[location];
-            call_debugger("GET D " + to_string(reg_d) + " <- " + to_string(location));
-        }
-        else if (instructions[i] == "3A")
-        {
+            call_debugger("GET D " + to_string(reg_d) + " <- " +
+                          to_string(location));
+        } else if (instructions[i] == "3A") {
             memory[location] = reg_a;
-            call_debugger("GET A " + to_string(reg_a) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "3B")
-        {
+            call_debugger("GET A " + to_string(reg_a) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "3B") {
             memory[location] = reg_b;
-            call_debugger("GET B " + to_string(reg_b) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "3C")
-        {
+            call_debugger("GET B " + to_string(reg_b) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "3C") {
             memory[location] = reg_c;
-            call_debugger("GET C " + to_string(reg_c) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "3D")
-        {
+            call_debugger("GET C " + to_string(reg_c) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "3D") {
             memory[location] = reg_d;
-            call_debugger("GET D " + to_string(reg_d) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "4A")
-        {
+            call_debugger("GET D " + to_string(reg_d) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "4A") {
             memory[location] = memory[location] + reg_a;
-            call_debugger("ADD A " + to_string(reg_a) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "4B")
-        {
+            call_debugger("ADD A " + to_string(reg_a) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "4B") {
             memory[location] = memory[location] + reg_b;
-            call_debugger("ADD B " + to_string(reg_b) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "4C")
-        {
+            call_debugger("ADD B " + to_string(reg_b) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "4C") {
             memory[location] = memory[location] + reg_c;
-            call_debugger("ADD C " + to_string(reg_c) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "4D")
-        {
+            call_debugger("ADD C " + to_string(reg_c) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "4D") {
             memory[location] = memory[location] + reg_d;
-            call_debugger("ADD D " + to_string(reg_d) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "5A")
-        {
+            call_debugger("ADD D " + to_string(reg_d) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "5A") {
             memory[location] = memory[location] - reg_a;
-            call_debugger("MIN A " + to_string(reg_a) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "5B")
-        {
+            call_debugger("MIN A " + to_string(reg_a) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "5B") {
             memory[location] = memory[location] - reg_b;
-            call_debugger("MIN B " + to_string(reg_b) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "5C")
-        {
+            call_debugger("MIN B " + to_string(reg_b) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "5C") {
             memory[location] = memory[location] - reg_c;
-            call_debugger("MIN C " + to_string(reg_c) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "5D")
-        {
+            call_debugger("MIN C " + to_string(reg_c) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "5D") {
             memory[location] = memory[location] - reg_d;
-            call_debugger("MIN D " + to_string(reg_d) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "6A")
-        {
+            call_debugger("MIN D " + to_string(reg_d) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "6A") {
             memory[location] = memory[location] * reg_a;
-            call_debugger("MUL A " + to_string(reg_a) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "6B")
-        {
+            call_debugger("MUL A " + to_string(reg_a) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "6B") {
             memory[location] = memory[location] * reg_b;
-            call_debugger("MUL B " + to_string(reg_b) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "6C")
-        {
+            call_debugger("MUL B " + to_string(reg_b) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "6C") {
             memory[location] = memory[location] * reg_c;
-            call_debugger("MUL C " + to_string(reg_c) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "6D")
-        {
+            call_debugger("MUL C " + to_string(reg_c) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "6D") {
             memory[location] = memory[location] * reg_d;
-            call_debugger("MUL D " + to_string(reg_d) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "7A")
-        {
+            call_debugger("MUL D " + to_string(reg_d) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "7A") {
             memory[location] = memory[location] / reg_a;
-            call_debugger("DIV A " + to_string(reg_a) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "7B")
-        {
+            call_debugger("DIV A " + to_string(reg_a) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "7B") {
             memory[location] = memory[location] / reg_b;
-            call_debugger("DIV B " + to_string(reg_b) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "7C")
-        {
+            call_debugger("DIV B " + to_string(reg_b) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "7C") {
             memory[location] = memory[location] / reg_c;
-            call_debugger("DIV C " + to_string(reg_c) + " -> " + to_string(location));
-        }
-        else if (instructions[i] == "7D")
-        {
+            call_debugger("DIV C " + to_string(reg_c) + " -> " +
+                          to_string(location));
+        } else if (instructions[i] == "7D") {
             memory[location] = memory[location] / reg_d;
-            call_debugger("DIV D " + to_string(reg_d) + " -> " + to_string(location));
+            call_debugger("DIV D " + to_string(reg_d) + " -> " +
+                          to_string(location));
         }
 
-        else if (instructions[i] == "AA")
-        {
+        else if (instructions[i] == "AA") {
             call_debugger("STP");
             nice_verbose_text::ok("CPU Stopped end of program");
             return 0;
-        }
-        else if (instructions[i] == "AB")
-        {
+        } else if (instructions[i] == "AB") {
             call_debugger("PRN " + to_string(location));
             cout << memory[location] << endl;
-        }
-        else if (instructions[i] == "BA")
-        {
+        } else if (instructions[i] == "BA") {
             call_debugger("PRN A");
             cout << reg_a << endl;
-        }
-        else if (instructions[i] == "BA")
-        {
+        } else if (instructions[i] == "BA") {
             call_debugger("PRN B");
-            cout << reg_b << endl; 
-        }
-        else if (instructions[i] == "BC")
-        {
+            cout << reg_b << endl;
+        } else if (instructions[i] == "BC") {
             call_debugger("PRN C");
             cout << reg_c << endl;
-        }
-        else if (instructions[i] == "BD")
-        {
+        } else if (instructions[i] == "BD") {
             call_debugger("PRN D");
             cout << reg_d << endl;
         }
-        if(TRACE){
+        if (TRACE) {
             cout << "======[REGISTERS]======" << endl;
             reg_dump(reg_a, reg_b, reg_c, reg_d);
             cout << "========[MEMORY]=======" << endl;
             memory_dump(memory);
         }
     }
-    nice_verbose_text::ok("Ran out of instructions to execute. CPU Stopped end of program");
+    nice_verbose_text::ok(
+        "Ran out of instructions to execute. CPU Stopped end of program");
     return 0;
 }
